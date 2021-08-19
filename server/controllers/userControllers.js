30cf2f31b029da7b3db9b33b030e2cbf077eb32c
Edgarwu <74028066+Edgarwu1984@users.noreply.github.com>
2021-08-19 22:06:03 +1000

    implement user register, login , logout functionaliy for backend. Installed Joi validation package for later validation implementation;

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,44 @@
-import { Users } from '../config/index.js';
+import { Users, Auth } from '../config/index.js';
+import Joi from 'joi';
 
 /* ============================ USER CONTROLLERS ============================ */
+const registerUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const authData = await Auth.createUserWithEmailAndPassword(email, password);
+
+    if (authData) {
+      res.status(201).send(authData);
+    } else {
+      res.status(400).send({ message: 'Error, not valid email or password' });
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const authData = await Auth.signInWithEmailAndPassword(email, password);
+    if (authData) {
+      res.status(200).send(authData);
+    } else {
+      res.status(400).send({ message: 'Error, not valid email or password' });
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+const logoutUser = async (req, res) => {
+  try {
+    await Auth.signOut();
+    res.status(200).send({ message: 'sign out.' });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
 /* ============================ ADMIN CONTROLLERS ============================ */
 // @description Get all Users
@@ -32,11 +70,10 @@ const getUserById = async (req, res) => {
     if (id && data) {
       res.status(200).json({ id, ...data });
     } else {
-      res.status(400);
-      throw new Error('User Not Found');
+      res.status(400).send({ message: 'User Not Found.' });
     }
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).send(error);
   }
 };
 
@@ -45,4 +82,11 @@ const getUserById = async (req, res) => {
 // @access Private/Admin
 const deleteUserById = async (req, res) => {};
 
-export { getUsers, getUserById, deleteUserById };
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUsers,
+  getUserById,
+  deleteUserById,
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 
 import {
+  loginUser,
+  logoutUser,
+  registerUser,
   getUserById,
   getUsers,
   deleteUserById,
@@ -11,5 +14,9 @@ const router = express.Router();
 router.route('/').get(getUsers);
 
 router.route('/:id').get(getUserById).delete(deleteUserById);
+
+router.route('/login').post(loginUser);
+router.route('/register').post(registerUser);
+router.route('/logout').post(logoutUser);
 
 export default router;
